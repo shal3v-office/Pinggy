be4159d4b964d2ceb606910639e2011eb603b1db
Rachel Weiss <shalev@atera.com>
2022-04-10 12:29:18 +0300

    try fetch with proxy

--- controllers/monitor-controller.js
+++ controllers/monitor-controller.js
@@ -1,32 +1,52 @@
 const monitorService = require("../services/monitor-service");
 const customerService = require("../services/customer-service");
 
+const addMonitorRow = async(url, status, siteId) => {
+    console.log("finally");
+    monitor =  {
+        url: url,
+        status:status,
+        site: siteId
+    };
+    const createdMonitor = await monitorService.addMonitorRow(monitor);
+    console.log(createdMonitor);
+};
+
 const runMonitor = async(req, res) => {
     try {
+        const monitorRows = [];
         const customers = await customerService.getAllCustomers();
-        customers.forEach(costomer => {
-            costomer.sites.forEach(site=>{
-                site.innerLinks.forEach(link=>{
+        for(const customer of customers){
+        // customers.forEach(costomer => {
+            for(const site of customer.sites){
+            // customer.sites.forEach(site=>{
+                for(const link of site.innerLinks){
+                // site.innerLinks.forEach(link=>{
                     var url = site.mainURL;
                     if(url.charAt(url.length - 1)!="/")url += "/";
                     url += link;
                     console.log(url);
-                    fetch(url).then((res) => {
+                    fetch("https://pinggy-app.herokuapp.com/"+url).then((res) => {
                         console.log("then");
-                      })
-                      .catch((err) => {
+                        console.log(res);
+                        monitorRows.push(addMonitorRow(url, 200, site._id));
+                    })
+                    .catch((err) => {
                         console.error("catch");
-                    }).finally( ()=>{console.log("finally")});
-                })
-            });
-        });
+                        console.log(err);
+                        monitorRows.push(addMonitorRow(url, 500, site._id));
+                    });
+                }
+            }
+        }
         // const monitor = req.body;
         // const createdMonitor = await monitorService.addMonitorRow(monitor);
-        return res.status(200).json(customers);
+        return res.status(200).json(monitorRows);
     } catch (err) {
         return res.status(500).send("Internal Server Error");
     }
 };
+// setInterval( runMonitor, 30 * 1000);
 
 const getFaildMonitorRowsBySite = async(req, res) => {
     try {
