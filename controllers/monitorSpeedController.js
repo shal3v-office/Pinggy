3ef10b3a2eee5f56f598765aa0fcaf130db009e9
Rachel Weiss <shalev@atera.com>
2022-05-03 10:24:32 +0300

    fix cronJobs

--- controllers/monitorSpeedController.js
+++ controllers/monitorSpeedController.js
@@ -1,37 +1,18 @@
 const monitorSpeedService = require("../services/monitorSpeedService");
-const customerService = require("../services/customerService");
-const siteService = require("../services/siteService");
-const getPageSpeedInsightPerURL = require('../lib/functions/getPageSpeedInsightPerURL.js');
-const scheduleSpeedMonitor = require('../lib/functions/scheduleSpeedMonitor');
+const getPageSpeedInsight = require('../lib/functions/getPageSpeedInsightPerURL.js');
+const scheduleSpeedMonitor = require('../lib/functions/cronJobs/scheduleSpeedMonitor');
 
 
 const runMonitorPerSite = async(req, res) => {
     try {
-        let monitorRows = [];
         let siteId = req.params.siteId;
-        const site = await siteService.getSiteById(siteId);
-        if(!site) throw new Error("The site is not valid");
-        var url = site.mainURL;
-        var checkedDate = new Date();
-        if(url.charAt(url.length - 1)!="/") url += "/";
-            for(const link of site.innerLinks){
-                var monitorUrl = url + link;
-                await getPageSpeedInsightPerURL(monitorUrl)
-                .then( async function (response) {
-                    //save the result in db
-                    let monitorRow = await monitorSpeedService.addMonitorRow({ siteId: siteId,link: monitorUrl,timestamps:{ createdAt: checkedDate },gpsiResult: response.result  });
-                    //schedule this every hour
-                    scheduleSpeedMonitor(siteId,monitorUrl);
-                    //save the object in array
-                    monitorRows.push(monitorRow);
-                })
-                .catch(function (error) {
-                    console.log("error in getPageSpeedInsightPerURL function");
-                    console.log(error.message);
-                })
-            }
+        const monitorRows = await getPageSpeedInsight(siteId).then( function(response) {
+            scheduleSpeedMonitor();
+            console.log(response)
+        });
         return res.status(200).json(monitorRows);
-    } catch (err) {
+    }
+    catch (err) {
         console.log(err);
         return res.status(500).send("Internal Server Error");
     }
